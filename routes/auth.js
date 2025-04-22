3e9963e4b024ed3b72ea7e52a4cb6ee12f89495f
ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-04-22 14:07:34 +0330

    clearing code

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { registerUser, findUser } = require("../models/User");
-const { createTask, getTasksByUserId } = require("../models/task");
 const router = express.Router();
 require("dotenv").config();
 
